19312f38b273bf16a97d8ab40610de694242d4f1
Thomas Zehnder <thommy@zeniba.fritz.box>
2020-10-03 12:13:37 +0200

    Bump version to 1.0

--- gw2_embeddings.php
+++ gw2_embeddings.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       GW2 Embeddings
  * Description:       Implements a shortcode for simplyfied use of the GW2 Armory embeddings
- * Version:           0.8.1
+ * Version:           1.0
  * Author:            guildnews.de
  * Author URI:        https://guildnews.de
  * License:           BSD-3 or later
