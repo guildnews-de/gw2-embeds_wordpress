32ee97b0f53dda1fa37aaa3a64c0848fc5a6e8bb
thom-10 <thomaszehnder@outlook.de>
2023-02-04 16:31:47 +0100

    implement build script

---
+++ packZip.js
@@ -0,0 +1,47 @@
+// Requires
+const fs = require("fs");
+const rimraf = require("rimraf");
+const dArch = require("dir-archiver");
+const path = require("path");
+
+const gw2Embeds = path.dirname(require.resolve("gw2-embeds"));
+const packStats = require("./composer.json");
+const dir = path.resolve("./dist");
+
+function fsError(err) {
+  if (err) {
+    return console.error(err);
+  }
+}
+
+// Prepare empty dist dir
+rimraf.sync(dir);
+fs.mkdirSync(dir, null, (err) => {
+  fsError(err);
+});
+fs.mkdirSync(`${dir}/raw`, null, (err) => {
+  fsError(err);
+});
+
+// Copy data
+fs.cpSync("./src", `${dir}/raw/`, { recursive: true }, (err) => {
+  fsError(err);
+});
+fs.cpSync(
+  gw2Embeds,
+  `${dir}/raw/public/gw2-embeds/`,
+  { recursive: true },
+  (err) => {
+    fsError(err);
+  }
+);
+
+// Build zip file
+const archive = new dArch(
+  `${dir}/raw`,
+  `${dir}/gw2-embeds_wp_${packStats.version}.zip`,
+  false,
+  []
+);
+
+archive.createZip();
